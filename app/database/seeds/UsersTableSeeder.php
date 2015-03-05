249fface19325a900305f53a891a9bcb2107884d
linezer0 <thomasparker@outlook.com>
2015-03-05 15:41:36 +0100

    Seed User Table

---
+++ app/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+// Composer: "fzaninotto/faker": "v1.3.0"
+use Faker\Factory as Faker;
+
+class UsersTableSeeder extends Seeder {
+
+	public function run()
+	{
+        User::create([
+            'email' => 'thomas@iaelyon.fr',
+            'password' => Hash::make('thomas'),
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime()
+        ]);
+	}
+
+}
